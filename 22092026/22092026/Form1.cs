4333b210742a0bfe41675db79682cfd7c773b221
kendinx <10atp630@gmail.com>
2026-09-22 12:14:00 +0300

    furkan
    hesap makinesi

---
+++ 22092026/22092026/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22092026
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("ilk mesaj","mesaj başlığı");
+            MessageBox.Show("2.mesaj","mesaj başlığı", MessageBoxButtons.YesNoCancel);
+            MessageBox.Show("3.mesaj","mesaj başlığı", MessageBoxButtons.OKCancel);
+        }
+        int a = 500;
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(a.ToString());
+            
+        }
+    }
+}  
---
+++ 22092026/22092026/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _22092026
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox2.Text);
+            toplam = sayi1 + sayi2;
+            textBox3.Text = toplam.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox2.Text);
+            toplam = sayi1 * sayi2;
+            textBox3.Text = toplam.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox2.Text);
+            toplam = sayi1 - sayi2;
+            textBox3.Text = toplam.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox2.Text);
+            toplam = sayi1 / sayi2;
+            textBox3.Text = toplam.ToString();
+        }
+    }
+}
